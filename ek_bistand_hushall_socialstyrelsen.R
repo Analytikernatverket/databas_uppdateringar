e41814f5b5dfa88f625c35c1658fcc215a819825
FaluPeppe <larspetermoller@gmail.com>
2025-10-05 00:35:15 +0200

    Ytterligare en buggfix hämta-skript för Socialstyrelsen som ska testas.

--- ek_bistand_hushall_socialstyrelsen.R
+++ ek_bistand_hushall_socialstyrelsen.R
@@ -12,7 +12,7 @@
 source("https://raw.githubusercontent.com/Region-Dalarna/funktioner/main/func_GIS.R", encoding = "utf-8", echo = FALSE)
 source("https://raw.githubusercontent.com/Analytikernatverket/hamta_data_playwright/main/hamta_ek_bistand_socialstyrelsen.R")
 
-args <- commandArgs(trailingOnly = TRUE)
+if (!exists("args")) args <- commandArgs(trailingOnly = TRUE)
 databas <- if (length(args) < 1) "oppna_data" else args[1]             # sparar data till databasen "oppna_data" om inte användaren skickar med ett annat databasnamn som argument till skriptet, i så fall används det istället
 schema_db <- if (length(args) < 2) "socialstyrelsen" else args[2]      # sparar till schema "socialstyrelsen" om det inte finns ett andra argument medskickat
 tabell_db <- if (length(args) < 3) "ek_bistand_hushall" else args[3]   # sparar till schema "ek_bistand_hushall" om det inte finns ett tredje argument medskickat
